43a341d6715a22f1ba1c9e6b83dced1dfad4f2ff
Parz6 <54594183+Parz6@users.noreply.github.com>
2019-08-27 13:34:46 -0400

    Initial commit

---
+++ src/main/java/rlbotexample/SamplePythonInterface.java
@@ -0,0 +1,16 @@
+package rlbotexample;
+
+import rlbot.Bot;
+import rlbot.manager.BotManager;
+import rlbot.pyinterop.SocketServer;
+
+public class SamplePythonInterface extends SocketServer {
+
+    public SamplePythonInterface(int port, BotManager botManager) {
+        super(port, botManager);
+    }
+
+    protected Bot initBot(int index, String botType, int team) {
+        return new SampleBot(index);
+    }
+}
